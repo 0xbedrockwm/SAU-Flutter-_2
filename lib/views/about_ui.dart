24612e0d9ec2a83917520ecefa58a9277bb2fe44
0xbedrockwm <wisanu.mangkala@gmail.com>
2026-02-20 21:06:10 +0700

    update bmi,bmr,calculator

--- lib/views/about_ui.dart
+++ lib/views/about_ui.dart
@@ -11,20 +11,47 @@ class _AboutUiState extends State<AboutUi> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.deepOrange,
-        title: Text(
-          'Body Health Calcuator',
-          style: TextStyle(
-            color: Colors.white,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 150.0,
+              ),
+              Text(
+                'Body Healt Calculator',
+                style: TextStyle(
+                  fontSize: 26.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 40.0),
+              Image.asset(
+                'assets/images/logo.png',
+                width: 150,
+                height: 150,
+                fit: BoxFit.cover,
+              ),
+              SizedBox(height: 40.0),
+              Text(
+                'คำนวนหาค่าดัชนีมวลกาย (BMI)',
+              ),
+              SizedBox(height: 30.0),
+              Text(
+                'คำนวนหาค่าแคลอรี่ที่ร่างกายต้องการต่อวัน (BMR)',
+              ),
+              SizedBox(height: 130.0),
+              Image.network(
+                'https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Logosau-02.png/250px-Logosau-02.png',
+                width: 80,
+                height: 80,
+              ),
+              Text('Deverloped By 0xFFFFFFFF 2026'),
+              SizedBox(height: 30.0),
+            ],
           ),
         ),
-        centerTitle: true,
       ),
-      body: Center(
-        child: Text('About'),
-      ),
-      backgroundColor: Colors.deepOrange,
     );
   }
 }
